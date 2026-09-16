c5e42a21159ba9cb6db4830fdbbebe8d4e97c447
super <jorgeasantiago904@gmail.com>
2026-09-15 17:16:51 -0700

    Add basic offline support with service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Basic offline support shell
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
